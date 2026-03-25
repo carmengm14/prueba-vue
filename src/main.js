8463eac33fbfb9d91623b00a053b92450b2897f7
carmengm14 <carmen2023@borealos.com>
2026-03-25 17:50:00 +0100

    Mis primeros cambios, creación de páginas y navbar con enrutado que las visualiza

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./styles/styles.css"
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
+
+/* 
+Esto hace tres cosas:
+- Crea la instancia principal de Vue
+- Le dice “usa este router”
+- Renderiza App.vue dentro del <div id="app"> del index.html
+Sin esta línea, Vue Router no funcionaría.
+*/
