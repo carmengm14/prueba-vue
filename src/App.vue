ce05aa58bfeba245f3f288cadc7664dc55f39b32
carmengm14 <carmen2023@borealos.com>
2026-03-26 17:17:57 +0100

    Typography change

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    
     <NavBar />
     <main class="contenido-web">
       <router-view/>
